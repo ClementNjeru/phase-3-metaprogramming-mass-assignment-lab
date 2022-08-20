dc6e2c2a19108e1d7962c02eaadf38229cc5e8f1
ClementNjeru <njeruclement36@gmail.com>
2022-08-21 00:30:04 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   
   def initialize(attributes = nil)
-    # if attributes
+    if attributes
       attributes.each do |key, value|
         self.class.attr_accessor(key)
         self.send("#{key}=", value)
